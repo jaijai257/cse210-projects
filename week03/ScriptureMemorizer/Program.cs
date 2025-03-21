09977280ea56608d4897b0a3ae583e9fcaa41da2
jaijai257 <jeffreyohaegbulem22@gmail.com>
2025-03-21 13:41:09 +0100

    Removed two scripture instances from the memorizer's verse list

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -9,8 +9,7 @@ class Program
             new Scripture("John 3:16", "For God so loved the world that he gave his only begotten son, that whosoever believes in him shall not perish but have eternal life."),
             new Scripture("Proverbs 3:5-6", "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight."),
             new Scripture("Philippians 4:13", "I can do all this through him who gives me strength."),
-            new Scripture("Romans 8:28", "And we know that in all things God works for the good of those who love him, who have been called according to his purpose."),
-            new Scripture("Isaiah 40:31", "But those who hope in the Lord will renew their strength. They will soar on wings like eagles; they will run and not grow weary; they will walk and not be faint.")
+            
         };
         Random random = new Random();
         bool continueGame = true;
